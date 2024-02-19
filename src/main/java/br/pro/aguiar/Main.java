6ca312b1afc1d5005d23573a91bbd53f768e0018
Prof. Thiago Vieira de Aguiar <56936784+thiago-aguiar@users.noreply.github.com>
2024-02-19 19:16:49 -0300

    Solicitando o Desconto do Produto via JOP.

--- src/main/java/br/pro/aguiar/Main.java
+++ src/main/java/br/pro/aguiar/Main.java
@@ -10,8 +10,10 @@ public class Main extends Thread {
         try {
             String nome = JOptionPane.showInputDialog("Informe o nome do produto:");
             String precoStr = JOptionPane.showInputDialog("Informe o preço do produto:");
-            double preco = Double.parseDouble(precoStr);
-            prod = new Produto(nome, preco, 0.01, 80);
+            double preco = Double.parseDouble(precoStr);            
+            String descontoStr = JOptionPane.showInputDialog("Informe o desconto do produto:");
+            double desconto = Double.parseDouble(descontoStr);
+            prod = new Produto(nome, preco, desconto, 80);
             var valorComDesconto = prod.getPrecoComDesconto();
             System.out.println(valorComDesconto);
             System.out.println(prod);
